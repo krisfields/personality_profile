26ef420cbf38ab04b55db3bffbecd7d0bc9f16be
Kris Fields <kris@avalaunch.com>
2012-09-08 17:21:15 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalityProfile::Application.config.secret_token = '820d020e508ce39f8b20ec82362af26f24b9d73885b51d5dba409b1df6a6dd4fd9c43b563c8fa7f3e8be3f6fa97694dc6758b1aaf994e8007e5e56ee470f4ddd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalityProfile::Application.config.session_store :cookie_store, key: '_personality_profile_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PersonalityProfile::Application.config.session_store :active_record_store
